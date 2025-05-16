2187e4e2c702376be2e62f0719d1efccde09e438
PJH <hong0921@gmail.com>
2025-05-16 10:14:15 +0900

    0516 프로젝트 commit

---
+++ SpringMVC6/src/main/java/com/lgdx/entity/Member.java
@@ -0,0 +1,21 @@
+package com.lgdx.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Member {
+	
+	@Id
+	private String id;
+	
+	private String pw;
+	
+	private String nick;
+	
+	private String addr;
+
+}
